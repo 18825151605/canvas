26b249c271e1a54f572e4257866931a6cff37e98
lzhibin <lin302010@qq.com>
2017-10-23 11:14:01 +0800

    lin

--- main.js
+++ main.js
@@ -1,29 +1,54 @@
 var canvas = document.querySelector('#canvas');
-var w = canvas.width = document.body.clientWidth;
-var h = canvas.height = document.body.clientWidth;
+var w = canvas.width = window.innerWidth;
+var h = canvas.height = window.innerHeight;
 var ctx = canvas.getContext('2d');
-var bedball = true;
-var red = {
-    x: parseInt(Math.random() * 500),//x随机数,
-    y: 0
+
+//生成红球的数组
+var redArr = [];
+function bornRed(num) {
+    for (var i = 0; i < num; i++) {
+        var json = {
+            x: parseInt(Math.random() * 500),//x随机数
+            y: parseInt(Math.random() * 100),
+            cansee: true
+        };
+        redArr.push(json);
+    }
 }
+bornRed(6);
+var getLongArr = [];
 var blue = {
     x: null,
     y: null,
+}
+function check() {
+    //获得两球的距离
+    // console.log(getLongArr);
+    for (var i = 0; i < redArr.length; i++) {
+        getLongArr[i] = Math.sqrt(Math.pow(Math.abs(ballCurY - redArr[i].y), 2) + Math.pow(Math.abs(ballCurX - redArr[i].x), 2));
+        if (getLongArr[i] < ball.radius * 2) {  //蓝球碰到红球 把bedball设置为false else true;
+            redArr[i].cansee = false;
+        }
+    }
+    // console.log(getLong);
+
 }
 var ball = {
     radius: 25,
     color: 'blue',
     rcolor: 'red', //掉落球颜色
-    draw: function (redX, redY, blueX, blueY) {
+    draw: function (blueX, blueY) {
         ctx.clearRect(0, 0, w, h);
-        if (bedball) {
-            ctx.beginPath();
-            ctx.arc(redX, redY, this.radius, 0, Math.PI * 2, true); //x,y变量
-            ctx.closePath();
-            ctx.fillStyle = 'red';
-            ctx.fill();
+        for (var k = 0; k < redArr.length; k++) {
+            if (redArr[k].cansee) {
+                ctx.beginPath();
+                ctx.arc(redArr[k].x, redArr[k].y, this.radius, 0, Math.PI * 2, true); //x,y变量
+                ctx.closePath();
+                ctx.fillStyle = 'red';
+                ctx.fill();
+            }
         }
+
         if (blueX) {
             ctx.beginPath();
             ctx.arc(blueX, blueY, this.radius, 0, Math.PI * 2, true);
@@ -44,14 +69,13 @@ canvas.onclick = function (e) {
     var tapX = e.clientX;
     var tapY = e.clientY;
     if (tapNum == 0) {
-        ball.draw(red.x, red.y, tapX, tapY);
+        ball.draw(tapX, tapY);
         ballCurX = tapX;
         ballCurY = tapY;
         canvas.onmousemove = function (e) {
-
             ballCurX = e.clientX;
             ballCurY = e.clientY;
-            ball.draw(red.x, red.y, ballCurX, ballCurY);
+            ball.draw(ballCurX, ballCurY);
         }
 
         tapNum = 2;
@@ -62,7 +86,7 @@ canvas.onclick = function (e) {
         getQ = true;
         tapNum = 3;
     } else if (tapNum == 3) {
-        console.log(tapX, ballCurX);
+        // console.log(tapX, ballCurX);
         var ballCur = {
             xmin: ballCurX - 25,
             xmax: ballCurX + 25,
@@ -78,10 +102,8 @@ canvas.onclick = function (e) {
                     qcy = tapY - ballCurY;
                     getQ = false;
                 }
-
-
-                console.log(tapX, tapY, qcx, qcy, ballCurX, ballCurY);
-                ball.draw(red.x, red.y, mX - qcx, mY - qcy);
+                // console.log(tapX, tapY, qcx, qcy, ballCurX, ballCurY);
+                ball.draw(mX - qcx, mY - qcy);
 
                 ballCurX = e.clientX - qcx;
                 ballCurY = e.clientY - qcy;
@@ -94,11 +116,9 @@ canvas.onclick = function (e) {
 
 
 setInterval(function () {
-    // red.y++;
-    // // if(){  //红球碰到篮球 红球消失
-
-    // // }else{
-
-    // // }
-    // ball.draw(red.x, red.y, ballCurX, ballCurY);
+    for (var i = 0; i < redArr.length; i++) {
+        redArr[i].y++;
+    }
+    check();
+    ball.draw(ballCurX, ballCurY);
 }, 10)
